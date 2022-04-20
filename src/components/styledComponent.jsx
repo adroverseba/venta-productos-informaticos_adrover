f007f110819c47b3b1841843ea845eec4ebe9246
adroverseba <adroversebastian.sa@gmail.com>
2022-04-20 11:39:24 -0300

    actualizar navbar

--- src/components/styledComponent.jsx
+++ src/components/styledComponent.jsx
@@ -162,6 +162,23 @@ const Price = styled.span`
   font-size: 40px;
 `;
 
+// export const ProductCountContainer = styled.div`
+//   width: 180px;
+//   margin: 0 auto;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   // border: solid grey 0.1px;
+//   border-radius: 10px;
+//   box-shadow: 0px 0px 0px 1px rgba(0, 0, 0, 0.2);
+// `;
+
+// export const MainProductContainer = styled.div`
+//   width: 300px;
+//   display: flex;
+//   flex-direction: column;
+// `;
+
 export {
   Logo,
   MenuItem,
